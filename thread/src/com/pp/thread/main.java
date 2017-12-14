bf98127b415167e23b60a5d85621167a5fe795c0
cyber4pp <472968264@qq.com>
2017-12-14 18:23:37 +0800

    17/12/4 thread study

--- thread/src/com/pp/thread/main.java
+++ thread/src/com/pp/thread/main.java
@@ -5,6 +5,21 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("hello world");
+			threadTest1 mt1=new threadTest1("1");
+			threadTest1 mt2=new threadTest1("2");
+			threadTest1 mt3=new threadTest1("3");
+			new Thread(mt1).start();
+			new Thread(mt2).start();
+			new Thread(mt3).start();
+			//内部函数
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					System.out.println("Hello World!");
+				}
+			}).start();
+			//lambda
+			new Thread(()-> System.out.println("Hello World ! 2")).start();
 	}
 
 }
--- thread/src/com/pp/thread/threadTest1.java
+++ thread/src/com/pp/thread/threadTest1.java
@@ -1,12 +1,14 @@
 package com.pp.thread;
 
 public class threadTest1 implements Runnable {
-	int i=0;
-	@Override
-	public synchronized void run() {
-		// TODO Auto-generated method stub
-		System.out.println(i++);
-		System.out.println(i);
+	private String title;
+	public threadTest1(String title){
+		this.title=title;
 	}
-	
+	@Override
+	public void run() {
+		for (int x=0;x<10;x++){
+			System.out.println(this.title+",x="+x);
+		}
+	}
 }
