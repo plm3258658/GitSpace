482599c7ce63518b6e03fcb5fd733399c4d32acc
cyber4pp <472968264@qq.com>
2018-01-26 18:17:48 +0800

    18/1/26 学前测试题1

---
+++ thread/src/com/study/dataStructure/Test1BeforeStudy.java
@@ -0,0 +1,62 @@
+package com.study.dataStructure;
+
+import java.util.Scanner;
+
+public class Test1BeforeStudy {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        StringBuffer sb=new StringBuffer(input.nextLine());
+        String[] str1=sb.toString().split("\\s+");
+        int sum=Integer.parseInt(str1[0]);
+        String symbol=str1[1];
+        int a=0;
+        int residue=0;
+        for(int i=1;i<=1000;i+=2){
+            if(i==1){
+                if(sum<=i){
+                    a=i;
+                }
+            }else if(i>1){
+                if(sum<=i*i-1){
+                    a=i;
+                    break;
+                }
+            }
+        }
+        if(a==1){
+            residue=sum-1;
+        }else{
+
+            residue=sum-(a*(a+1)/2+(a-1)/2);
+        }
+        for(int i=1;i<=a;i++){
+                if(i==1 || i==a){
+                    for(int s=1;s<=a;s++){
+                        System.out.print(symbol);
+                    }
+                }else if(i<=(a+1)/2){
+                    for(int z=1;z<=i-1;z++){
+                        System.out.print(" ");
+                    }
+                    for(int x=1;x<=a-(i-1)*2;x++){
+                        System.out.print(symbol);
+                    }
+                    for(int c=1;c<=i-1;c++){
+                        System.out.print(" ");
+                    }
+                }else if(i>(a+1)/2){
+                    for(int z=1;z<=(a-i);z++){
+                        System.out.print(" ");
+                    }
+                    for(int x=1;x<=(i-(a+1)/2)*2+1;x++){
+                        System.out.print(symbol);
+                    }
+                    for(int c=1;c<=(a-i);c++){
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+        }
+        System.out.println(residue);
+    }
+}
